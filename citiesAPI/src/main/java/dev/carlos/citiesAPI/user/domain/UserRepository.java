cabec4eb7fa7d4ed809b71b7dcd01b8ad0f8a16a
Carlos Eduardo Pagan <92696898+carlospagan@users.noreply.github.com>
2022-10-13 11:30:07 -0300

    Delete citiesAPI directory

--- citiesAPI/src/main/java/dev/carlos/citiesAPI/user/domain/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.carlos.citiesAPI.user.domain;
-
-import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Mono;
-
-public interface UserRepository extends ReactiveMongoRepository<User, String> {
-}
